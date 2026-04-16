cadb9de92a1a3ed6503c7e20e0e6ab7228288356
grigorii.khlystov <khlystov.ga@phystech.edu>
2026-04-16 17:02:12 +0300

    Add per-channel zero probability logging

--- src/net_complexity/metrics/stg.py
+++ src/net_complexity/metrics/stg.py
@@ -1,10 +1,10 @@
-from ..models.feature_selection import get_gumbel_modules
+from ..models.feature_selection import get_stg_modules
 from ._channel_prob import ChannelZeroProbMetric
 
 
-class GumbelProbMetric(ChannelZeroProbMetric):
+class STGProbMetric(ChannelZeroProbMetric):
     def __init__(self, log_channel_zero_probs: bool = True):
         super().__init__(
-            get_gumbel_modules,
+            get_stg_modules,
             log_channel_zero_probs=log_channel_zero_probs,
         )
